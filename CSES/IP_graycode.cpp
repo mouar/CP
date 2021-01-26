5931c1c7d0160dccffc1e699b6cb117cb918bbdb
shubham mouar <mouar1998@gmail.com>
2021-01-26 17:03:45 +0530

    Introductory Problems CSES

---
+++ CSES/IP_graycode.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+
+// https://www.geeksforgeeks.org/generate-n-bit-gray-codes/
+// https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
+
+#define int long long int
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) ((a > b) ? b : a)
+#define mod 1e9 + 7
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+#define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+#define INF 1e18
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)          //no of zero's before the first 1 from the right
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define w(x)            int x; cin>>x; while(x--)
+#define F first
+#define S second
+#define PB push_back   //queue
+#define POPF pop_front
+#define PF push_front  //deque
+#define POPB pop_back
+#define MP make_pair
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count()); //random shuffler
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //Policy based data structure
+
+void s_m()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("error.txt", "w", stderr);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+int32_t main()
+{
+	s_m();
+	int n;
+	cin >> n;
+	// for genetating normal binary string
+	// string s(n, '0');
+	// cout << s << "\n";
+	// for (int i = 1; i <= n; i++)
+	// {
+	// 	string str1(n - i, '0');
+	// 	string str2(i, '1');
+	// 	string str = str1 + str2;
+	// 	do {
+	// 		cout << str << "\n";
+	// 	}
+	// 	while (next_permutation(str.begin(), str.end()));
+	// }
+
+	vector<string> v;
+	v.push_back("0");
+	v.push_back("1");
+	if (n == 1)
+	{
+		for (auto it : v)
+			cout << it << "\n";
+	}
+	else
+	{
+		for (int i = 2; i < (1 << n); i = i << 1)
+		{
+			for (int j = i - 1; j >= 0; j--)
+			{
+				v.push_back(v[j]);
+			}
+			// for (auto i : v)
+			// 	cout << i << "\n";
+			for (int j = 0; j < i; j++)
+			{
+				v[j] += "0";
+			}
+			for (int j = i; j < 2 * i; j++)
+			{
+				v[j] += "1";
+			}
+		}
+		for (auto i : v)
+			cout << i << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
